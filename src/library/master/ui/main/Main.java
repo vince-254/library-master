943afeddc67dc034add44c1ff8137ea21560647a
vince-254 <vmiriga@gmail.com>
2020-02-26 08:02:50 +0300

    Library management system

---
+++ src/library/master/ui/main/Main.java
@@ -0,0 +1,29 @@
+package library.master.ui.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Library Master");
+        
+       
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
